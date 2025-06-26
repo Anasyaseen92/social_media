bf588cf725cb8fad625af06ea7418fd99cd9a406
Anasyaseen92 <anasy7148@gmail.com>
2025-06-26 13:54:16 -0700

    work on backend

--- Server/index.js
+++ Server/index.js
@@ -8,6 +8,11 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register  } from "./controller/auth";
+import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js"
+import {createPost} from "./controller/posts.js"
 
 /* CONFIGURATION */
 const __filename = fileURLToPath(import.meta.url);
@@ -34,6 +39,16 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts",verifyToken, upload.single("picture"), createPost);
+
+/* ROUTES */
+
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
